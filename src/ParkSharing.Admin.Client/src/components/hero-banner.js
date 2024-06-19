5cf6f471186c43207ad826fa1fabc59061a31b43
David Rosko <rosko.dav@gmail.com>
2024-06-19 13:18:49 +0200

    Add buttons for share and reserve

--- src/ParkSharing.Admin.Client/src/components/hero-banner.js
+++ src/ParkSharing.Admin.Client/src/components/hero-banner.js
@@ -1,19 +1,21 @@
-// File: ./src/ParkSharing.Admin/ParkSharing.Admin.Client/src/components/hero-banner.js
-
 import React from "react";
-import { SignupButton } from "./buttons/signup-button";
-import { LoginButton } from "./buttons/login-button";
+import { useNavigate } from "react-router-dom";
 
 export const HeroBanner = () => {
   const logo = "https://cdn.auth0.com/blog/developer-hub/react-logo.svg";
+  const navigate = useNavigate();
 
   return (
     <div className="hero-banner hero-banner--pink-yellow">
-        <img className="hero-banner__image" src={`${process.env.PUBLIC_URL}/logo.svg`} alt="Logo" />
+      <img className="hero-banner__image" src={`${process.env.PUBLIC_URL}/logo.svg`} alt="Logo" />
       <h1 className="hero-banner__headline">Sdílej svoje parkovací místo!</h1>
       <div className="hero-banner__buttons">
-        {/* <SignupButton /> */}
-        {/* <LoginButton /> */}
+        <button className="button__reserve" onClick={() => window.location.href = "https://parksharing.obseum.cloud"}>
+          Rezervovat
+        </button>
+        <button className="button__share" onClick={() => navigate("/settings")}>
+          Sdílet
+        </button>
       </div>
     </div>
   );
