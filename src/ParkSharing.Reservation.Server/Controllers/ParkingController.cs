019faf7b43182732529c02e0739f3cc0e26cdc7b
David Rosko <rosko.dav@gmail.com>
2024-06-24 20:03:42 +0200

    Add sessions store

--- src/ParkSharing.Reservation.Server/Controllers/ParkingController.cs
+++ src/ParkSharing.Reservation.Server/Controllers/ParkingController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenAI.ObjectModels.RequestModels;
+using ParkSharing.Reservation.Server.Services.Session;
+using ParkSharing.Reservation.Server.Services.Session.Model;
 using ParkSharing.Services.ChatGPT;
 using ParkSharing.Services.ChatGPT.Helpers;
 using System.Security.Cryptography;
@@ -10,25 +12,27 @@ public class ParkingController : ControllerBase
 {
     private readonly HttpClient _httpClient;
     ChatGPTService _gpt;
-    static List<ChatMessage> messages = new List<ChatMessage>();
     ILogger<ParkingController> _log;
+    ISessionService _sessionsService;
 
-    public ParkingController(HttpClient httpClient, ChatGPTService gpt, ILogger<ParkingController> log)
+    public ParkingController(HttpClient httpClient, ChatGPTService gpt, ILogger<ParkingController> log, ISessionService sessionsService)
     {
         _httpClient = httpClient;
         _gpt = gpt;
         _log = log;
+        _sessionsService = sessionsService;
     }
 
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] UserInputModel input)
     {
-        var sessionId = GetOrCreateSessionId();
+        var session = await GetOrCreateSession();
         try
         {
-            messages.Add(ChatMessage.FromUser(Helpers.SanitizeHtml(input.Input)));
-            messages = await _gpt.Send(messages);
-            return Ok(new { reply = messages.LastOrDefault().Content });
+            session.Messages.Add(ChatMessage.FromUser(Helpers.SanitizeHtml(input.Input)));
+            var newMessages = await _gpt.Send(session.Messages);
+            await _sessionsService.UpdateAllMessages(session.PublicId, newMessages);
+            return Ok(new { reply = newMessages.LastOrDefault().Content });
         }
         catch (Exception ex)
         {
@@ -37,18 +41,26 @@ public class ParkingController : ControllerBase
         }
     }
 
-    private string GetOrCreateSessionId()
+    private async Task<ParkSharing.Reservation.Server.Services.Session.Model.Session> GetOrCreateSession()
     {
+        ParkSharing.Reservation.Server.Services.Session.Model.Session session;
         var sessionId = HttpContext.Session.GetString("SessionId");
-
-        // If not present, generate a new one and set it in the session
         if (string.IsNullOrEmpty(sessionId))
         {
             sessionId = GenerateSecureSessionId();
             HttpContext.Session.SetString("SessionId", sessionId);
+            session = await _sessionsService.CreateSession(sessionId);
         }
+        else
+        {
+            session = await _sessionsService.GetSession(sessionId);
 
-        return sessionId;
+            if(session == null)
+            {
+                session = await _sessionsService.CreateSession(sessionId);
+            }
+        }
+        return session;
     }
 
     private string GenerateSecureSessionId()
---
+++ src/ParkSharing.Reservation.Server/Services/Session/Model/Session.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using OpenAI.ObjectModels.RequestModels;
+
+namespace ParkSharing.Reservation.Server.Services.Session.Model
+{
+    public class Session
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string Id { get; set; }
+
+        [BsonElement("PublicId")]
+        public string PublicId { get; set; }
+
+        [BsonElement("Messages")]
+        public List<ChatMessage> Messages { get; set; }
+    }
+}
