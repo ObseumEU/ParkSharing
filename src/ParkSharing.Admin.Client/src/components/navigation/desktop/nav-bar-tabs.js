886752888c5ae13e175073a2ddf94fc4fc3fae7e
David Rosko <rosko.dav@gmail.com>
2024-07-17 21:34:54 +0200

    Add support and feedback

--- src/ParkSharing.Admin.Client/src/components/navigation/desktop/nav-bar-tabs.js
+++ src/ParkSharing.Admin.Client/src/components/navigation/desktop/nav-bar-tabs.js
@@ -5,16 +5,25 @@ import { NavBarTab } from "./nav-bar-tab";
 export const NavBarTabs = () => {
   const { isAuthenticated } = useAuth0();
 
+  const handleRoadmapClick = () => {
+    window.open("https://parksharingobseum.featurebase.app/roadmap", "_blank");
+  };
+
+  const handlePatreonClick = () => {
+    window.open("https://www.patreon.com/fomodog/membership", "_blank");
+  };
+
   return (
     <div className="nav-bar__tabs">
       {/* <NavBarTab path="/profile" label="Profile" /> */}
       {/* <NavBarTab path="/public" label="Public" /> */}
       {isAuthenticated && (
         <>
+          <div className="nav-bar__tab" onClick={handlePatreonClick}>Nakrm vývojáře</div>
           <NavBarTab path="/reservations" label="Rezervace" />
           <NavBarTab path="/protected" label="Dostupnost" />
           <NavBarTab path="/settings" label="Nastavení" />
-
+          <div className="nav-bar__tab" onClick={handleRoadmapClick}>Feedback</div>
           {/* <NavBarTab path="/admin" label="Admin" /> */}
         </>
       )}
