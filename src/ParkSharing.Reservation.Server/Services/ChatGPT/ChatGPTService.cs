191b5e682d56ef180a2c234a1854e510734cb160
David Roško <rosko.dav@gmail.com>
2024-10-04 21:12:22 +0200

    Tweak instructions

--- src/ParkSharing.Reservation.Server/Services/ChatGPT/ChatGPTService.cs
+++ src/ParkSharing.Reservation.Server/Services/ChatGPT/ChatGPTService.cs
@@ -162,7 +162,7 @@ namespace ParkSharing.Services.ChatGPT
             newList.Insert(0, ChatMessage.FromSystem("Nelze rezervovat více časů najednou."));
             newList.Insert(0, ChatMessage.FromSystem("Všechna parkování jsou venkovní."));
             newList.Insert(0, ChatMessage.FromSystem("Nepomáhej s ničím, co není uvedeno v instrukcích."));
-            newList.Insert(0, ChatMessage.FromSystem("Pis velice jasne instrukce."));
+            newList.Insert(0, ChatMessage.FromSystem("Pis velice jasne instrukce a buď stručnej. Používej ascii obrázky"));
 
             newList.Insert(0, ChatMessage.FromSystem("Místo pro rezervaci musí být dostupné po celou dobu rezervace."));
             newList.Insert(0, ChatMessage.FromUser("Sdílení a rezervace parkovacích míst ve Velvarii! \ud83d\ude97 Stačí napsat na kdy chcete místo rezervovat."));
